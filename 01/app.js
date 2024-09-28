18251bd83dda529b4a8e620553b8c48d8fb2dae1
GrzybowskiPiotr <grzybowski_piotr@hotmail.com>
2024-09-28 19:13:34 +0200

    done

--- 01/app.js
+++ 01/app.js
@@ -1,21 +1,45 @@
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    const divList = document.querySelectorAll('div');
-    
-    setBorderColorAsync(divList[0], 'red', function() {
-        setBorderColorAsync(divList[1], 'blue', function() {
-            setBorderColorAsync(divList[2], 'green', function() {
-                console.log('finish');
-            });
-        });
+  const divList = document.querySelectorAll("div");
+
+  setBorderColorAsync(divList[0], "red")
+    .then(() => {
+      setBorderColorAsync(divList[1], "blue");
+    })
+    .then(() => {
+      setBorderColorAsync(divList[2], "green");
+    })
+    .then(() => console.log("finish"))
+    .catch((error) => {
+      console.error(error);
     });
 
+  // setBorderColorAsync(divList[0], 'red', function() {
+  //     setBorderColorAsync(divList[1], 'blue', function() {
+  //         setBorderColorAsync(divList[2], 'green', function() {
+  //             console.log('finish');
+  //         });
+  //     });
+  // });
 }
 
-function setBorderColorAsync(element, color, callback) {
-    setTimeout(() => {
+function setBorderColorAsync(element, color) {
+  return new Promise((resolve, reject) => {
+    if (!element) {
+      reject("brak elementu");
+    } else {
+      setTimeout(() => {
         element.style.border = `3px solid ${color}`;
-        callback();
-    }, Math.random() * 3000);
+        resolve();
+      }, Math.random() * 3000);
+    }
+  });
 }
+
+// function setBorderColorAsync(element, color, callback) {
+//   setTimeout(() => {
+//     element.style.border = `3px solid ${color}`;
+//     callback();
+//   }, Math.random() * 3000);
+// }
