53293f3e8ac69a591ad654621a172f2c72e133aa
GrzybowskiPiotr <grzybowski_piotr@hotmail.com>
2024-09-28 19:38:00 +0200

    done

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,16 @@
-document.addEventListener('DOMContentLoaded', init);
-
+document.addEventListener("DOMContentLoaded", init);
+const button = document.querySelector("button");
+const span = document.querySelector("span");
 function init() {
-    console.log('DOM');
+  console.log("DOM");
 }
+
+async function handleButtonClick() {
+  const url = "https://api.ipify.org?format=json";
+  fetch(url)
+    .then((res) => res.json())
+    .then((res) => (span.innerHTML = res.ip))
+    .catch((error) => console.error(error));
+}
+
+button.addEventListener("click", handleButtonClick);
